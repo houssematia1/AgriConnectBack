698c396882122cb79a7a3d5cf0f06bfdd296c9df
arij5555 <arij.hedhri@esprit.tn>
2025-04-25 10:45:34 +0100

    Create Produit.java

---
+++ src/main/java/com/example/usermanagementbackend/entity/Produit.java
@@ -0,0 +1,27 @@
+package com.example.usermanagementbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
+    private String numeroDeTelephone;
+    private String role;
+    private String adresseLivraison;
+
+
+  
+}
